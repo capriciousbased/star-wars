f358a1b1d90f912f41a90d1a54fe64709cf381d6
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-04-21 04:38:05 +0200

    planete data fetched done

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { DataGrid } from '@material-ui/data-grid';
 
-const Planets = () => {
+const Planets = ({data}) => {
+    const columns = [
+        { field: 'name', headerName: 'Name', width: 150 },
+        { field: 'diameter', headerName: 'Diameter', width: 100 },
+        { field: 'climate', headerName: 'Climate', width: 100 },
+        { field: 'gravity', headerName: 'Gravity', width: 200 },
+        
+      ];
+      
+      let rows = []
+      data.forEach (planet => rows.push({ id: planet.name, name: planet.name, diameter: planet.diameter, climate: planet.climate, gravity:planet.gravity}))
+    
+
     return (
-        <div>
-            <h2>Planets</h2>
-        </div>
+        <div style={{ height: '800px', width: '100%' }}>
+      <h3 style={{marginLeft: "15px"}}>Users Data</h3>
+      <DataGrid 
+        id={Math.floor(Math.random())} 
+        rows={rows} 
+        columns={columns} 
+        pageSize={10} 
+        checkboxSelection
+        onRowSelected={() => {console.log('row selected')}}
+        />
+    </div>
     )
 }
 
